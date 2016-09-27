a5fff539e53e85bb7de8f0b1e736014b9da5e556
Portalshade <spoonmage@live.com>
2016-09-27 19:36:48 +0100

    meerrrhhh

--- Source/SeaForest/CreatureBase.cpp
+++ Source/SeaForest/CreatureBase.cpp
@@ -4,29 +4,24 @@
 #include "CreatureBase.h"
 
 
-// Sets default values
 ACreatureBase::ACreatureBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
-// Called when the game starts or when spawned
 void ACreatureBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void ACreatureBase::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
 
 }
 
-// Called to bind functionality to input
 void ACreatureBase::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 {
 	Super::SetupPlayerInputComponent(InputComponent);
